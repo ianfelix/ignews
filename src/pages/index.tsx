f7af430a5a39e9b3be69db903fb2bb7d35514979
ianfelix <ianfelix030@gmail.com>
2021-03-31 00:19:54 -0300

    feat: created home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -7,9 +8,20 @@ export default function Home() {
         <title>Home | ig.news</title>
       </Head>
 
-      <h1>
-        Hello <span>World!</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9,90 month</span>
+          </p>
+        </section>
+
+        <img src='/images/avatar.svg' alt='' />
+      </main>
     </>
   );
 }
