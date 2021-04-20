05eee5567f330f76d638e77b87b9aa9af46a58b8
ianfelix <ianfelix030@gmail.com>
2021-04-20 08:33:04 -0300

    feat: session token generated

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,25 @@
 import styles from './styles.module.scss';
+import { useSession, signIn } from 'next-auth/client';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const [session] = useSession();
+
+  const handleSubscribe = () => {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+  };
+
   return (
-    <button type='button' className={styles.subscribeButton}>
+    <button
+      type='button'
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}>
       Subscribe now
     </button>
   );
