178fa4b82be2554262891f3171425d0e021f180f
ianfelix <ianfelix030@gmail.com>
2021-04-20 09:09:06 -0300

    feat: redirect to stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 import styles from './styles.module.scss';
 import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -8,10 +10,22 @@ interface SubscribeButtonProps {
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
   const [session] = useSession();
 
-  const handleSubscribe = () => {
+  const handleSubscribe = async () => {
     if (!session) {
       signIn('github');
       return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error);
     }
   };
 
